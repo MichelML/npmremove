45c703d81bc471b229710235bfeda9fd44a9f868
MichelML <michmoreau.l@gmail.com>
2017-12-19 17:30:51 -0500

    run only if main module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-if (require.main !== module) {
+if (require.main === module) {
     const { rm } = require('shelljs');
     rm('-rf', `node_modules/${process.argv[process.argv.length - 1]}`)
 }
